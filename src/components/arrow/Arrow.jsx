2ddb85e115e59d2821860f7493d6ba7657cef638
Deathvks <dylanjesussuarez@gmail.com>
2024-04-09 19:50:02 +0100

    add effect arrow

--- src/components/arrow/Arrow.jsx
+++ src/components/arrow/Arrow.jsx
@@ -1,24 +1,42 @@
+import React, { useEffect, useState } from 'react'; // Importa useEffect y useState
 import "./Arrow.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
-// import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 function Arrow() {
+    const [isVisible, setIsVisible] = useState(true); // Estado para controlar la visibilidad
 
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
         });
-        
     }
 
+    useEffect(() => {
+        let lastScrollTop = 0; // Variable para almacenar la última posición de desplazamiento
+
+        const onScroll = () => {
+            let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+            if (currentScroll > lastScrollTop) {
+                // Desplazamiento hacia abajo
+                setIsVisible(false);
+            } else {
+                // Desplazamiento hacia arriba
+                setIsVisible(true);
+            }
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Actualiza la última posición de desplazamiento
+        };
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => window.removeEventListener('scroll', onScroll); // Limpieza al desmontar
+    }, []);
+
     return (
-        <div className="arrow-container">
-            <FontAwesomeIcon onClick={scrollToTop} className="header-icons-desktop" icon={faArrowUp} />
-            {/* <i class="fa-solid fa-arrow-up fa-xs" style="color: #fff2cd;"></i> */}
+        <div onClick={scrollToTop} className={`arrow-container ${isVisible ? '' : 'hidden'}`}>
+            <FontAwesomeIcon className="header-icons-desktop" icon={faArrowUp} />
         </div>
     )
 }
