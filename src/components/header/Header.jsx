7b39fcf1004b2be0345e56a27b24108933eb2648
Deathvks <dylanjesussuarez@gmail.com>
2024-04-13 17:54:57 +0100

    hover header decoration

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ function Header(){
                     <a href='./' className='header-a'><h1 className='header-name'>EL REFUGIO DEL GOURMET</h1></a>
                 </div>
                 <nav>
-                    <a href='./'>Inicio</a>
+                    <a href='./' className='header-active'>Inicio</a>
                     <a href=''>Sobre Nosotros</a>
                     <a href='/menu'>Carta</a>
                     <a href='https://www.instagram.com' target='blank'><img src="../../../img/insta.webp" alt='insta' className='header-insta'/></a>
