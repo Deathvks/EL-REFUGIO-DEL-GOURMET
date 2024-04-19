4b98ca80c419c76838a08a57369e1e9ceda75b4e
Deathvks <dylanjesussuarez@gmail.com>
2024-04-19 22:50:59 +0100

    fixed bugs

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,11 +2,13 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Arrow from "../../components/arrow/Arrow";
 import AbouContent from "../../components/about-content/AboutContent";
+import Route from "../../components/route/Route";
 function About(){
     return(
         <div>
             <Header />
             <AbouContent/>
+            <Route />
             <Footer />
             <Arrow />
         </div>
