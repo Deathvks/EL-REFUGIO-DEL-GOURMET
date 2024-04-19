59371006273e9c83a60882078c67d63f50adc35b
Deathvks <dylanjesussuarez@gmail.com>
2024-04-19 20:40:11 +0100

    fixed bugs

---
+++ src/components/about-content/AboutContent.jsx
@@ -0,0 +1,18 @@
+import "./AboutContent.css";
+
+function AboutContent() {
+    return (
+        <div className="about-content-all">
+            <div className="about-container-presentation">
+                <div className="about-content-text">
+                    <h1>Prueba</h1>
+                </div>
+                <div className="about-content-image">
+                    <img className="about-content-image1" src="/img/about.webp" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutContent;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import Arrow from "../../components/arrow/Arrow";
+import AbouContent from "../../components/about-content/AboutContent";
 function About(){
     return(
         <div>
             <Header />
+            <AbouContent/>
             <Footer />
             <Arrow />
         </div>
