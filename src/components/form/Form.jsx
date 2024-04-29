ad7e4bd49f0029d77b4c4dc1559d42639e2a8424
Deathvks <dylanjesussuarez@gmail.com>
2024-04-29 22:10:53 +0100

    add alert

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,44 +3,65 @@ import RateService from "../../services/firebase/rates.service.js";
 import "./Form.css";
 
 function Form() {
-
     const [name, setName] = useState("");
     const [rate, setRate] = useState("");
+    const [alert, setAlert] = useState("");
+    const [alertType, setAlertType] = useState("");
 
     const changeName = (e) => {
-        setName(e.target.value)
-    }
+        setName(e.target.value);
+    };
 
     const changeRate = (e) => {
-        setRate(e.target.value)
-    }
+        setRate(e.target.value);
+    };
 
     const addNewRate = (e) => {
         e.preventDefault();
-
-        saveRate(e.target.name.value, e.target.rate.value);
-    }
+        if (!name.trim() || !rate.trim()) {
+            setAlert("Los campos no pueden estar vacíos");
+            setAlertType("error-message");
+            setTimeout(() => {
+                setAlert("");
+            }, 3000);
+            return;
+        }
+        saveRate(name, rate);
+    };
 
     const saveRate = (name, rate) => {
-        RateService.addRate(name, rate).then((response) => {
-            console.log("guardado")
-        })
-    }
+        RateService.addRate(name, rate).then(() => {
+            setAlert("Reseña añadida con éxito");
+            setAlertType("success-message");
+            setName("");
+            setRate("");
+            setTimeout(() => {
+                setAlert("");
+            }, 3000);
+        }).catch(error => {
+            setAlert("Error al guardar la reseña");
+            setAlertType("error-message");
+            setTimeout(() => {
+                setAlert("");
+            }, 3000);
+            console.error(error);
+        });
+    };
 
     return (
         <>
-            <p>{alert}</p>
+             {alert && <p className={alertType}>{alert}</p>}
             <form onSubmit={addNewRate}>
                 <label htmlFor="name" className="name">Nombre:</label>
-                <input type="text" id="name" name="name" value={name} onChange={changeName}/>
+                <input type="text" id="name" name="name" value={name} onChange={changeName} className={alert ? "error" : ""}/>
 
                 <label htmlFor="rate" className="rate">Reseña:</label>
-                <input type="text" id="rate" name="rate" value={rate} onChange={changeRate}/>
+                <input type="text" id="rate" name="rate" value={rate} onChange={changeRate} className={alert ? "error" : ""}/>
 
                 <button type="submit" className="sub">Añadir reseña</button>
             </form>
         </>
-    )
+    );
 }
 
 export default Form;
