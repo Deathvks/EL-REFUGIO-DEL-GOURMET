758e55be8d80da53e5a46d5d696ef3c4db6bf532
Deathvks <dylanjesussuarez@gmail.com>
2024-05-08 10:43:54 +0100

    New Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,41 @@
-import './Header.css';
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className='header-container'>
-            <header>
-                <div className='header-presentation'>
-                    <a href='./' className='header-a'><img src="../../img/refugioDelGourmet.png" alt="logo" className='header-logo' /></a>
-                    <a href='./' className='header-a'><h1 className='header-name'>EL REFUGIO DEL GOURMET</h1></a>
-                </div>
-                <nav>
-                    <a href='./' className='header-active'>Inicio</a>
-                    <a href='/about'>Sobre Nosotros</a>
-                    <a href='/menu'>Carta</a>
-                </nav>
-            </header>
+  return (
+    <div className="header-container">
+      <header>
+        <div className="header-presentation">
+          <a href="./" className="header-a">
+            <img
+              src="../../img/refugioDelGourmet.png"
+              alt="logo"
+              className="header-logo"
+            />
+          </a>
+          <a href="./" className="header-a">
+            <h1 className="header-name">EL REFUGIO DEL GOURMET</h1>
+          </a>
         </div>
-    )
+        <nav className="nav-container">
+          <a href="./">
+            <button className="button type1">
+              <span className="btn-txt">Inicio</span>
+            </button>
+          </a>
+          <a href="/about">
+            <button className="button type1">
+              <span className="btn-txt">Nosotros</span>
+            </button>
+          </a>
+          <a href="/menu">
+            <button className="button type1">
+              <span className="btn-txt">Carta</span>
+            </button>
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
